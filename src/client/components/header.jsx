c70fc664d49eda5df414b06f5a41ac63e44a2528
rlemon <rob.lemon@gmail.com>
2016-12-05 14:54:04 -0500

    well that was stupid

--- src/client/components/header.jsx
+++ src/client/components/header.jsx
@@ -5,9 +5,9 @@ import { Menu, Col } from 'antd';
 
 import logoImage from '../logo.png';
 
-export default class TopMenu extends Component {
+export default class Header extends Component {
     
-    static contentTypes = {
+    static contextTypes = {
         currentUser: React.PropTypes.object
     }
 
@@ -26,25 +26,32 @@ export default class TopMenu extends Component {
     }
     render() {
         const links = [];
-        console.log('rendering header');
+        // this is pretty bad, I know it. I'm still not sure about the nav containers I can use. 
+        // so I'm cheating / being lazy and just fucking with the styles here
+        // don't judge me, I'm already dead. 
         if( this.props.loggedIn ) {
-            if( this.context.currentUser ) {
-                links.push(
-                <Menu.Item key="profile">
-                    <Link to="/profile">
-                        profile <FancyImage src={this.context.currentUser.avatar_url} style={{borderRadius: '50%', width: '48px' }}/>
-                    </Link>
-                </Menu.Item>
-                );
-            }
             links.push(
-            <Menu.Item key="logout" style={{float: 'right'}}>
-                <Link to="/logout">logout</Link>
-            </Menu.Item>
-            );
+                <Menu.Item key="logout" style={{float: 'right'}}>
+                    <Link to="/logout">logout</Link>
+                </Menu.Item>);
+            links.push(
+                <Menu.Item key="profile" style={{float: 'right'}}>
+                    <Link to="/profile">
+                        profile <FancyImage 
+                                    src={this.context.currentUser.avatar_url} 
+                                    style={
+                                            {
+                                                borderRadius: '50%', 
+                                                width: '28px', 
+                                                padding: '0 2px', 
+                                                verticalAlign: 'middle'
+                                            }
+                                    }/>
+                    </Link>
+                </Menu.Item>);
         } else {
             links.push(
-            <Menu.Item key="login">
+            <Menu.Item key="login" style={{float: 'right'}}>
                 <Link to="/login">login</Link>
             </Menu.Item>
             );
